52d8c280d2cc01f229d2af9f3297e1c8bae0c25e
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-08-26 17:49:57 -0500

    installed react-native-app-auth and implementing it in auth component

--- trackutane/components/Auth/Auth.tsx
+++ trackutane/components/Auth/Auth.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {authorize} from 'react-native-app-auth';
+
+// base config
+const config = {
+  issuer: '<YOUR_ISSUER_URL>',
+  clientId: '<YOUR_CLIENT_ID>',
+  redirectUrl: '<YOUR_REDIRECT_URL>',
+  scopes: ['<YOUR_SCOPE_ARRAY>'],
+};
+
+// use the client to make the auth request and receive the authState
+try {
+  const result = await authorize(config);
+  // result includes accessToken, accessTokenExpirationDate and refreshToken
+} catch (error) {
+  console.log(error);
+}
 
 const Auth = () => {
   return (
