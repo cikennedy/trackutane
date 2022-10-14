c109582ca3373e2fcd2ee6ace8169ff6d820b867
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-14 13:42:35 -0500

    tailwind config

--- trackutane/screens/PatientsScreen.tsx
+++ trackutane/screens/PatientsScreen.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import { useTailwind } from "tailwind-rn";
 
 const PatientsScreen = () => {
+  const tailwind = useTailwind();
+
   return (
-    <View>
-      <Text>PatientsScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={tailwind("text-red-500")}>PatientsScreen</Text>
+    </SafeAreaView>
   );
 };
 
