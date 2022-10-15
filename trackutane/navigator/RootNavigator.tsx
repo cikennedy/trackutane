1c13e1bf21352f52164009a237167581448bc023
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-15 10:03:20 -0500

    navigator components

--- trackutane/navigator/RootNavigator.tsx
+++ trackutane/navigator/RootNavigator.tsx
@@ -1,14 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./TabNavigator";
 
-const Stack = createNativeStackNavigator();
+// this is all in the documentation
+export type RootStackParamList = {
+  // no props so we aren't going to pass any value
+  Main: undefined;
+  MyModal: { userId: string; name: string };
+  Visit: { visit: any };
+};
+
+const RootStack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <View>
-      <Text>RootNavigator</Text>
-    </View>
+    <RootStack.Navigator>
+      <RootStack.Group>
+        <RootStack.Screen name="Main" component={TabNavigator} />
+      </RootStack.Group>
+    </RootStack.Navigator>
   );
 };
 
