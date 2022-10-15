5df8835edb2b2d47752fbf43f55813eb0f777251
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-15 10:13:08 -0500

    screen creation and tab navigator imports

--- trackutane/navigator/TabNavigator.tsx
+++ trackutane/navigator/TabNavigator.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import PatientsScreen from "../screens/PatientsScreen";
+import VisitsScreen from "../screens/VisitsScreen";
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Patients" component={PatientsScreen} />
+      <Tab.Screen name="Visits" component={VisitsScreen} />
+    </Tab.Navigator>
   );
 };
 
