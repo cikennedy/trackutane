b7b24f714f9dfeb45dc3ed9dadf738ce00329b73
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-16 09:26:00 -0500

    removing header in navigator

--- trackutane/navigator/TabNavigator.tsx
+++ trackutane/navigator/TabNavigator.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import PatientsScreen from "../screens/PatientsScreen";
 import VisitsScreen from "../screens/VisitsScreen";
+import { useNavigation } from "@react-navigation/native";
 
 export type TabStackParamList = {
   Patients: undefined;
@@ -11,6 +12,15 @@ export type TabStackParamList = {
 const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
+  const navigation = useNavigation();
+
+  // get rid of header, can also do this at the root nagivator level, however it is cleaner here
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
     <Tab.Navigator>
       <Tab.Screen name="Patients" component={PatientsScreen} />
