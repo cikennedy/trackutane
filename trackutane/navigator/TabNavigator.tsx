99185c14f2de33f089b966071a7df482a8dbb69d
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-16 10:52:10 -0500

    tab navigation, icon imports

--- trackutane/navigator/TabNavigator.tsx
+++ trackutane/navigator/TabNavigator.tsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import PatientsScreen from "../screens/PatientsScreen";
 import VisitsScreen from "../screens/VisitsScreen";
 import { useNavigation } from "@react-navigation/native";
+import { Icon } from "@rneui/themed";
 
 export type TabStackParamList = {
   Patients: undefined;
@@ -22,7 +23,31 @@ const TabNavigator = () => {
   }, []);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Patients") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "59C1CC" : "gray"}
+              />
+            );
+          } else if (route.name === "Visits") {
+            return (
+              <Icon
+                name="calendar"
+                type="entypo"
+                color={focused ? "EB6A7C" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Patients" component={PatientsScreen} />
       <Tab.Screen name="Visits" component={VisitsScreen} />
     </Tab.Navigator>
