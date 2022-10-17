c70f05d20baf0f277f50fd2574b82e892a94fa9e
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-17 10:02:21 -0500

    composite navigation prop, navigation in patients screen

--- trackutane/screens/PatientsScreen.tsx
+++ trackutane/screens/PatientsScreen.tsx
@@ -1,14 +1,33 @@
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import { useTailwind } from "tailwind-rn";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigator/RootNavigator";
+
+// nested stacks, composite navigation prop
+// combine the navigation items with all the types needed
+export type PatientScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Patients">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const PatientsScreen = () => {
   const tailwind = useTailwind();
 
+  const navigation = useNavigation();
+
   return (
-    <SafeAreaView>
-      <Text style={tailwind("text-blue-600")}>PatientsScreen</Text>
-    </SafeAreaView>
+    <ScrollView>
+      <SafeAreaView>
+        <Text style={tailwind("text-blue-600")}>PatientsScreen</Text>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
