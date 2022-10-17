3becdc5cd132857917df7a6cdfaa7d55e934573f
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-17 11:38:57 -0500

    removed header in visits screen, added types and imports

--- trackutane/screens/VisitsScreen.tsx
+++ trackutane/screens/VisitsScreen.tsx
@@ -1,11 +1,41 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useTailwind } from "tailwind-rn";
+import { Image } from "@rneui/themed";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from "../navigator/TabNavigator";
+
+export type PatientScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Patients">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const VisitsScreen = () => {
+  const tailwind = useTailwind();
+
+  const navigation = useNavigation<PatientScreenNavigationProp>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>VisitsScreen</Text>
-    </View>
+    <ScrollView>
+      <Image
+        source={{
+          uri: "https://github.com/cikennedy/trackutane-typescript/blob/main/trackutane/assets/undraw_Booking_re_gw4j.png?raw=true",
+        }}
+        containerStyle={tailwind("2-full h-64")}
+      />
+    </ScrollView>
   );
 };
 
