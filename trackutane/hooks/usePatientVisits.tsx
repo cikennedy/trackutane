df05a7b7f4b1575255445490a8827919e8279e57
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-19 15:27:28 -0500

    usePatientVisits hook

--- trackutane/hooks/usePatientVisits.tsx
+++ trackutane/hooks/usePatientVisits.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_VISITS } from "../graphql/queries";
 
 const usePatientVisits = () => {
+  const { loading, error, data } = useQuery(GET_VISITS);
+  const [visits, setVisits] = useState<Visit[]>([]);
+
+  useEffect(() => {}, []);
+
   return <div>usePatientVisits</div>;
 };
 
