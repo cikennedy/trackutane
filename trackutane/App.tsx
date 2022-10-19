47058d4e6f9843e2bc1b491f8327531b1a389f23
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-19 11:15:40 -0500

    importing apollo client

--- trackutane/App.tsx
+++ trackutane/App.tsx
@@ -1,8 +1,18 @@
 import { TailwindProvider } from "tailwind-rn";
 import utilities from "./tailwind.json";
 import { NavigationContainer } from "@react-navigation/native";
-
 import RootNavigator from "./navigator/RootNavigator";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/eyewitness-ibis",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
