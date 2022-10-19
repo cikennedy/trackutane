81b1cdbae25b044ebba58538b4a3742d7316a4f3
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-19 11:51:02 -0500

    graphql queries, building out directoreis and file structure

---
+++ trackutane/graphql/queries.ts
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+// graphql queries
+// TO-DO! Fill out
+export const GET_PATIENTS = gql`
+    query GetPatients {
+        
+    }
+`;
+
+export const GET_VISITS = gql`
+    query GetPatients {
+        
+    }
+`;
