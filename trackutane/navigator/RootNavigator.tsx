e8d939aaac9da362e6c52196bd113ef34077ed77
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-21 16:47:48 -0500

    modal screen rnfe

--- trackutane/navigator/RootNavigator.tsx
+++ trackutane/navigator/RootNavigator.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigator from "./TabNavigator";
+import ModalScreen from "../screens/ModalScreen";
 
 // this is all in the documentation
 export type RootStackParamList = {
@@ -11,13 +12,25 @@ export type RootStackParamList = {
   Visit: { visit: any };
 };
 
-const RootStack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
   return (
     <RootStack.Navigator>
       <RootStack.Group>
         <RootStack.Screen name="Main" component={TabNavigator} />
+      </RootStack.Group>
+
+      <RootStack.Group
+        screenOptions={{
+          presentation: "modal",
+        }}
+      >
+        <RootStack.Screen
+          options={{ headerShown: false }}
+          name="MyModal"
+          component={ModalScreen}
+        />
       </RootStack.Group>
     </RootStack.Navigator>
   );
