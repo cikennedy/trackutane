a6560333f44130bc2cafc93e7700a3d1f941abe2
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-21 16:57:42 -0500

    type creation for modal screen navigation prop

--- trackutane/screens/ModalScreen.tsx
+++ trackutane/screens/ModalScreen.tsx
@@ -2,13 +2,27 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Icon } from "@rneui/themed";
 import { useTailwind } from "tailwind-rn/dist";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigator/RootNavigator";
+
+type ModalScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList>,
+  NativeStackNavigationProp<RootStackParamList, "MyModal">
+>;
 
 const ModalScreen = () => {
   const tailwind = useTailwind();
+  const navigation = useNavigation();
 
   return (
     <View>
-      <TouchableOpacity style={tailwind("absolute right-5 top-5")}>
+      <TouchableOpacity style={tailwind("absolute right-5 top-5 z-10")}>
         <Icon name="closecircle" type="antdesign" />
       </TouchableOpacity>
     </View>
