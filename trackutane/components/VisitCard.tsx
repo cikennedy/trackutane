0ccb6e9d190ecbacf7b47efa99db2b32414d3baa
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-10-25 16:27:31 -0500

    styling visit card

--- trackutane/components/VisitCard.tsx
+++ trackutane/components/VisitCard.tsx
@@ -1,15 +1,33 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useTailwind } from "tailwind-rn/dist";
+import { Card, Icon } from "@rneui/themed";
 
 type Props = {
   visit: Visit;
 };
 
 const VisitCard = ({ visit }: Props) => {
+  const tailwind = useTailwind();
+
   return (
-    <View>
-      <Text>VisitCard</Text>
-    </View>
+    <Card
+      containerStyle={[
+        tailwind("rounded-lg my-2"),
+        {
+          padding: 0,
+          paddingTop: 16,
+          shadowColor: "black",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+        },
+      ]}
+    >
+      <View>
+        <Icon name="calendar" type="entypo" color="black" />
+      </View>
+    </Card>
   );
 };
 
